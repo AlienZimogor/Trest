4971c04268da732dc1a74c10dfddba8742e941e7
AlienZimogor <40263590+AlienZimogor@users.noreply.github.com>
2026-09-17 13:48:01 +0300

    Update vfi_mini.cpp

--- vfi_mini.cpp
+++ vfi_mini.cpp
@@ -96,10 +96,10 @@ int main(int argc, char** argv) {
     ncnn::Net net;
     net.opt.use_vulkan_compute = use_gpu;
     if (use_gpu) net.set_vulkan_device(0);
-    net.opt.use_fp16_packed = false;
-    net.opt.use_fp16_storage = false;
+    net.opt.use_fp16_packed = true;
+    net.opt.use_fp16_storage = true;
     net.opt.use_fp16_arithmetic = false;
-    net.opt.use_packing_layout = false;
+    net.opt.use_packing_layout = true;
     net.opt.num_threads = 4;
     net.register_custom_layer("rife.Warp", Warp_layer_creator);
     if (net.load_param(param) != 0) {
